baa7beea8bff3a89cc022d49dc931191a208e0ea
Brian Barbosa <barbosab@users.noreply.github.com>
2025-06-05 14:58:14 -0400

    Update get game API to return character

--- lambda/get_game.py
+++ lambda/get_game.py
@@ -1,11 +1,20 @@
 import boto3
+import decimal
 import json
 import os
 
-def handler(event, context):
-    game_id = event.get("queryStringParameters", {}).get("game_id")
-    print(f"Game ID: {game_id}")
+def decimal_default(obj):
+    if isinstance(obj, decimal.Decimal):
+        # Convert to int if possible, else float
+        return int(obj) if obj % 1 == 0 else float(obj)
+    raise TypeError
 
+def handler(event, context):
+    claims = event['requestContext']['authorizer']['jwt']['claims']
+    user_id = claims.get('sub')
+
+    #  Get game_id from query parameters
+    game_id = event.get("queryStringParameters", {}).get("game_id")
     if not game_id:
         return {
             'statusCode': 400,
@@ -17,11 +26,22 @@ def handler(event, context):
 
     # Initialize DynamoDB client
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('FW_Sagas_Dev')
+
+    # Retrieve user data from DynamoDB
+    user_table = dynamodb.Table('FW_UserData_Dev')
+    user_data_response = user_table.get_item(Key={'user_id': user_id})
+    user_record = user_data_response['Item']
+    
+    character_profile = None
+    for character in user_record.get('characters', []):
+        if character.get('active_games')[0] == game_id:
+            character_profile = character
+
+    print(f"Character profile for game {game_id}: {character_profile}")
 
     # Retrieve the game record from DynamoDB
+    table = dynamodb.Table('FW_Sagas_Dev')
     response = table.get_item(Key={'game_id': game_id})
-    print(f"Response from DynamoDB: {response}")
 
     if 'Item' not in response:
         return {
@@ -33,11 +53,12 @@ def handler(event, context):
         }
 
     game_record = response['Item']
+    game_record['character_profile'] = character_profile
     print(f"Game record: {game_record}")
 
     return {
         'statusCode': 200,
-        'body': json.dumps(game_record),
+        'body': json.dumps(game_record, default=decimal_default),
         'headers': {
             'Content-Type': 'application/json'
         }
